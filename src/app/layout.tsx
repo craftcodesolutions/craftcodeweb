51138907cfbc041bf3acbd0d57ba63321e63e0cb
Awkwardd-code <somethinn999awkwardd@gmail.com>
2025-07-16 00:15:13 +0600

    hello

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 // import ModeToggle from "@/components/ModeToggle"; // Remove this import
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
+  variable: "--font-roboto",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"], // You can adjust weights as needed
 });
 
 export const metadata: Metadata = {
@@ -27,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {/* <div className="fixed top-4 right-4 z-[1000]">
