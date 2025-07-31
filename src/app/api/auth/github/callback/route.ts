6be3ceb76b7f9e0fdd9680894995260374b4e9df
Awkwardd-code <somethinn999awkwardd@gmail.com>
2025-08-01 02:04:21 +0600

    not

--- src/app/api/auth/github/callback/route.ts
+++ src/app/api/auth/github/callback/route.ts
@@ -87,7 +87,7 @@ export async function GET(req: NextRequest) {
     );
 
     // 7. Redirect
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000';
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? 'https://www.craftcodesolutions.com';
     const res = NextResponse.redirect(`${baseUrl}/`);
 
     res.cookies.set('authToken', token, {
--- src/app/api/auth/google/redirect/route.ts
+++ src/app/api/auth/google/redirect/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    const redirect_uri = 'http://localhost:3000/api/auth/google/callback';
+    const redirect_uri = `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/google/callback`;
 
     const params = new URLSearchParams({
         client_id: process.env.GOOGLE_CLIENT_ID!, // Must match the Console
