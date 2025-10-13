6316571e4f2c858eddab7e0575072e8c3c43d80e
Awkwardd-code <somethinn999awkwardd@gmail.com>
2025-10-13 13:42:08 +0600

    token

--- src/app/(auth)/token/route.ts
+++ src/app/(auth)/token/route.ts
@@ -1,10 +1,14 @@
-// /app/api/auth/token/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   const token = req.cookies.get('authToken')?.value;
+
   if (!token) {
-    return NextResponse.json({ token: null }, { status: 401 });
+    return NextResponse.json(
+      { token: null, message: 'No authToken cookie found' },
+      { status: 401 }
+    );
   }
+
   return NextResponse.json({ token });
 }
