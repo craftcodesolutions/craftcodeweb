3e838d393dbf4b87b77f2e2f0d36cd2401b84381
Awkwardd-code <somethinn999awkwardd@gmail.com>
2025-10-21 14:53:02 +0600

    Project Successfull

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,11 @@ import { ThemeProvider } from "next-themes";
 import { AuthProvider } from "@/context/AuthContext";
 import { DisabledAccountsProvider } from "@/context/DisabledAccountsContext";
 import { GlobalChatProvider } from "@/context/GlobalChatContext";
-// import FloatingChatButton from "@/components/FloatingChatButton";
 import GlobalChatBox from "@/components/GlobalChatBox";
 import NotificationPermission from "@/components/NotificationPermission";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Script from "next/script";
 
 // import ModeToggle from "@/components/ModeToggle"; // Remove this import
 
@@ -32,6 +32,30 @@ export default function RootLayout({
         className={`${roboto.variable} antialiased`}
         suppressHydrationWarning
       >
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1373668294154666');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <img height="1" width="1" style="display:none"
+                src="https://www.facebook.com/tr?id=1373668294154666&ev=PageView&noscript=1"
+                alt=""
+              />
+            `,
+          }}
+        />
         <ThemeProvider 
           attribute="class" 
           defaultTheme="system" 
