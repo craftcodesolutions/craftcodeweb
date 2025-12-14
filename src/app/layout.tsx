c4aae1e5398b1df61fda8bf6e47440c69139ee96
Awkwardd-code <somethinn999awkwardd@gmail.com>
2025-12-14 11:06:53 +0600

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import NotificationPermission from "@/components/NotificationPermission";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Script from "next/script";
+import { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
 
 // import ModeToggle from "@/components/ModeToggle"; // Remove this import
 
@@ -19,7 +20,40 @@ const roboto = Roboto({
   subsets: ["latin"],
   weight: ["400", "700"], // You can adjust weights as needed
 });
-
+export const metadata: Metadata = {
+  title: "CraftCodeSloutions || Startup & SME Investment Studio",
+  description:
+    "A Startup & SME Investment Studio. 360 degree Marketing Agency.Your growth team. We build, market & invest in startups.",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  openGraph: {
+    title: "CraftCodeSloutions || Startup & SME Investment Studio",
+    description:
+      "A Startup & SME Investment Studio. 360 degree Marketing Agency.Your growth team. We build, market & invest in startups.",
+    url: "https://www.craftcodesolutions.com",
+    siteName: "CraftCodeSloutions",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "CraftCodeSloutions",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "CraftCodeSloutions || Startup & SME Investment Studio",
+    description:
+      "A Startup & SME Investment Studio. 360 degree Marketing Agency.Your growth team. We build, market & invest in startups.",
+    images: ["/og-image.jpg"],
+  },
+};
 
 
 export default function RootLayout({
