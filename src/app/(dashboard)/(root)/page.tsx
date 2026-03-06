bff0ad843817d7c4b65ed85a92c3933d98481510
Awkwardd-code <somethinn999awkwardd@gmail.com>
2026-03-06 16:00:08 +0600

    not

--- src/app/(dashboard)/(root)/page.tsx
+++ src/app/(dashboard)/(root)/page.tsx
@@ -2,27 +2,27 @@ import React from 'react'
 import HomeSection from './_components/HomeSection'
 import FeaturedSection from './_components/FeaturedSection'
 import ServicesSection from './_components/ServicesSection'
-import TestimonialSection from './_components/TestimonialSection'
+// import TestimonialSection from './_components/TestimonialSection'
 import WhyUsSection from './_components/WhyUsSection'
 import WhyToUsSection from './_components/WhyToUsSection'
 import CoreTechSection from './_components/CoreTechSection'
-import SupportSection from './_components/SupportSection'
+// import SupportSection from './_components/SupportSection'
 import BlogSection from './_components/BlogSection'
 import CTASection from './_components/CTASection'
-import TestimonialsSlider from './_components/TestimonialsSlider'
+// import TestimonialsSlider from './_components/TestimonialsSlider'
 
 const Home = () => {
   return (
     <div>
       <HomeSection />
       <FeaturedSection />
-      <TestimonialsSlider/>
+      {/* <TestimonialsSlider/> */}
       <ServicesSection />
-      <TestimonialSection/>
+      {/* <TestimonialSection/> */}
       <WhyUsSection/>
       <WhyToUsSection/>
       <CoreTechSection/>
-      <SupportSection/>
+      {/* <SupportSection/> */}
       <BlogSection/>
       <CTASection/> 
       
